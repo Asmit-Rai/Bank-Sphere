085d179b10f2f33127f2a1611d5ca1c1eebd4e4e
Asmit-Rai <211b075@juetguna.co.in>
2025-02-19 03:55:55 +0530

    refactor: improve layout structure and fix type errors; add new components for balance display

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,19 @@
 import type { Metadata } from "next";
-import {Inter , IBM_Plex_Serif } from "next/font/google";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-
-const inter = Inter({subsets:["latin"], variable:'--font-inter'});
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const ibmPlexSerif = IBM_Plex_Serif({
-  subsets:['latin'],
-  weight:['400','700'],
-  variable:'--font-ibm-plex-serif'
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
 })
 
 export const metadata: Metadata = {
-  title: "bank-sphere",
-  description: "Bank Sphere is a banking plateform for everyone.",
-  icons:
-  {
-    icon:'/icons/logo.svg'
+  title: "Bank Sphere",
+  description: "Bank Sphere is a modern banking platform.",
+  icons: {
+    icon: '/icons/logo.svg'
   }
 };
 
@@ -26,11 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} ${ibmPlexSerif.variable}`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
     </html>
   );
 }
