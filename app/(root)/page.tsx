63edd0fd63080971848e36234768bd210b9b1bc9
Asmit Rai <er.asmitrai@gmail.com>
2025-03-07 11:58:23 +0530

    Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react';
 const Home = ()=>
 {
-    const loggedIn = {firstName:'Asmit'};
+    const loggedIn = {firstName:'User'};
     return (
        <section className='home'>
         <div className='home-content'>
@@ -16,7 +17,13 @@ const Home = ()=>
                 />
                 <TotalBalanceBox accounts={[]} totalBanks ={1} totalCurrentBalance = {1250.35}/>
             </header>
+            Recent Transactions
         </div>
+        <RightSidebar
+        user={loggedIn}
+        transaction={[]}
+        bank={[]}
+        />
        </section>
     )
 }
